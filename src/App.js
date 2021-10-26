c5aaaab0e663f9c610fc194a2ab92f8208edd545
Damian <damianrossi@live.com.ar>
2021-10-26 15:06:19 +0200

    Created component, basic styling and mock data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import mockData from "./mockData";
+import './app.scss';
+// import './app.scss';
 
 function App() {
+  const taskGroups = mockData;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+        <h1> Logdify Grouped Tasks  </h1>
+        <span className="app-progressBar"></span>
       </header>
+      <section className="app-content">
+        {
+          taskGroups.map(group => (
+            <div className="app-accordion">
+              <header className="app-contentHeader"> 
+              <h2><i class="fa fa-file-contract" aria-hidden="true"></i> Group 1</h2>
+              </header>
+                <ul className="app-groupList">
+                  <li className="app-groupItem">
+                      <ul className="app-itemList">
+                        <li>
+                          <span className="app-toggle">
+                          </span>
+                            Task 2-1
+                          </li>
+                          <li>
+                            <span className="app-toggle">
+                            </span>
+                              Task 2-2
+                          </li>
+                      </ul>
+                  </li>
+                </ul>
+            </div>
+          ))
+        }
+      </section>
     </div>
   );
 }
-
 export default App;
---
+++ src/mockData.js
@@ -0,0 +1,94 @@
+const mockData = [
+  {
+    name: "General Infos",
+    tasks: [
+      {
+        description: "Add name and surname",
+        value: 10,
+        checked: true
+      },
+      {
+        description: "Add email",
+        value: 15,
+        checked: false
+      },
+      {
+        description: "Add linkedin profile",
+        value: 8,
+        checked: false
+      },
+      {
+        description: "Provide websites page url",
+        value: 5,
+        checked: true
+      }
+    ]
+  },
+  {
+    name: "Accomiplishment",
+    tasks: [
+      {
+        description: "Wrote a small poem about the birthdate",
+        value: 23,
+        checked: false
+      },
+      {
+        description: "Jump three time with one leg",
+        value: 32,
+        checked: true
+      },
+      {
+        name: "Avoid the annoying neighbor",
+        value: 2,
+        checked: false
+      },
+      {
+        name: "Say hello to a random person",
+        value: 21,
+        checked: false
+      },
+      {
+        name: "Fill the description in at least 3 places",
+        value: 12,
+        checked: true
+      }
+    ]
+  },
+  {
+    name: "Personal retrospective",
+    tasks: [
+      {
+        description: "Remember a dream",
+        value: 15,
+        checked: true
+      },
+      {
+        description: "Have a crush",
+        value: 7,
+        checked: true
+      }
+    ]
+  },
+  {
+    name: "Things before leaving",
+    tasks: [
+      {
+        description: "Tell what you feel to all the people you know",
+        value: 42,
+        checked: false
+      },
+      {
+        description: "Have a pet",
+        value: 23,
+        checked: false
+      },
+      {
+        description: "Buy a fashion shirt",
+        value: 12,
+        checked: false
+      }
+    ]
+  }
+]
+
+export default mockData;
